98a7d0b237ceb445f2b14600e2d7cd64a225f362
akerk <akerkeolzhatay@gmail.com>
2024-09-20 16:15:33 +0500

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Square {
+    double height;
+
+    public Square(double height) {
+        this.height = height;
+    }
+
+    public double area() {
+        return (this.height * this.height);
+    }
+}
+class Action{
+    public double compareArea(Square a, Square b){
+        return a.area() - b.area();
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Square a = new Square(2);
+        Square b = new Square(3);
+        Action action = new Action();
+        System.out.println(action.compareArea(a, b));
     }
 }
